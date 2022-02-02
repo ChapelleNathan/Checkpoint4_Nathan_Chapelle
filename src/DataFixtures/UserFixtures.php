a8e8feb78627d0315b4429555dc105045bec48fe
Chapelle Nathan <nathanchapelle76@gmail.com>
2022-02-02 17:09:56 +0100

    user fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public const USERS = 4;
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i < self::USERS; $i++) {
+            $user = new User();
+            $user->setEmail($faker->email());
+            $user->setFirstname($faker->firstName());
+            $user->setLastname($faker->lastName());
+            $user->setPseudo($user->getFirstname() . '_' . $user->getLastname());
+            $user->setPassword($this->passwordHasher->hashPassword($user, $faker->password()));
+            $this->addReference('user_' . $i, $user);
+            $manager->persist($user);
+        }
+
+        $john = new User();
+        $john->setEmail('john@doe.com');
+        $john->setFirstname('John');
+        $john->setLastname('Doe');
+        $john->setPseudo('John_Doe');
+        $john->setPassword($this->passwordHasher->hashPassword($john, 'whoami'));
+        $this->setReference('user_john', $john);
+        $manager->persist($john);
+
+        $manager->flush();
+    }
+}
