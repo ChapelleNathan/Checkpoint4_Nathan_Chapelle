d3331904b8e818ad185065ebd7d883b8e408aa91
Chapelle Nathan <nathanchapelle76@gmail.com>
2022-02-03 12:19:37 +0100

    can upload

--- src/Entity/Posts.php
+++ src/Entity/Posts.php
@@ -35,7 +35,7 @@ class Posts
     private $pictureFile;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $description;
 
---
+++ src/Form/PostsType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Posts;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+
+class PostsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pictureFile', VichFileType::class, [
+                'required' => true,
+                'allow_delete' => false,
+                'download_uri' => false,
+            ])
+            ->add('description', TextType::class,[
+                'required' => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Posts::class,
+        ]);
+    }
+}
